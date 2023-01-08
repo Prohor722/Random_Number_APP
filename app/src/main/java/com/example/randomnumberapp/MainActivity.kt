6c94d410efb9dc26a3079ab73c2c503734ff20a2
Prohor722 <prohor.b722@gmail.com>
2023-01-09 02:03:11 +0600

    complete

--- app/src/main/java/com/example/randomnumberapp/MainActivity.kt
+++ app/src/main/java/com/example/randomnumberapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.randomnumberapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.generateButton)
+
+        btn.setOnClickListener {
+            rand()
+        }
+    }
+
+    private fun rand(){
+
+        val textView:TextView = findViewById(R.id.textArea)
+
+        Toast.makeText(this,"Random Number Generated", Toast.LENGTH_LONG).show()
+        var numb = (1..10).random()
+        textView.text = numb.toString()
     }
 }
